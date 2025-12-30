021f7f74c7e0d41ae37ed0eabdb26e4f033569b7
ARYANGODxDOfficial <aryansanjay.080805@gmail.com>
2025-12-30 08:21:21 +0000

    sm6375-common: Bringup for EuclidOs

--- common.mk
+++ common.mk
@@ -294,7 +294,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/euclid/interfaces/power-libperfmgr \
     hardware/motorola \
     hardware/qcom-caf/common/libqti-perfd-client
 
